ff283084a99102574b01a73614828cb07b879061
Attar Maviya Mujib <149134437+maviyaattar@users.noreply.github.com>
2026-07-07 04:27:33 -0700

    Update Masjid.js

--- models/Masjid.js
+++ models/Masjid.js
@@ -3,103 +3,110 @@ const mongoose = require("mongoose");
 const MasjidSchema = new mongoose.Schema({
 
     name:{
-
         type:String,
-
         required:true,
-
         trim:true
+    },
 
+    image:{
+        type:String,
+        default:""
     },
 
     area:{
-
         type:String,
-
         required:true,
-
         trim:true
-
     },
 
     address:{
-
         type:String,
-
         required:true,
-
         trim:true
+    },
 
+    description:{
+        type:String,
+        default:""
     },
 
     imam:{
-
         type:String,
-
         default:""
-
     },
 
     contact:{
-
         type:String,
-
         default:""
-
     },
 
-    latitude:{
-
-        type:Number,
-
-        required:true
-
+    googleMapLink:{
+        type:String,
+        default:""
     },
 
-    longitude:{
-
-        type:Number,
-
-        required:true
-
-    },
+    facilities:[{
+        type:String,
+        trim:true
+    }],
 
     prayerTimes:{
 
-        fajr:String,
+        fajr:{
+            type:String,
+            default:""
+        },
 
-        zuhr:String,
+        sunrise:{
+            type:String,
+            default:""
+        },
 
-        asr:String,
+        zuhr:{
+            type:String,
+            default:""
+        },
 
-        maghrib:String,
+        asr:{
+            type:String,
+            default:""
+        },
 
-        isha:String,
+        maghrib:{
+            type:String,
+            default:""
+        },
 
-        jumuah:String
+        isha:{
+            type:String,
+            default:""
+        },
 
+        jumuah:{
+            type:String,
+            default:""
+        }
+
+    },
+
+    establishedYear:{
+        type:Number,
+        default:null
     },
 
     admin:{
-
         type:mongoose.Schema.Types.ObjectId,
-
         ref:"User",
-
         default:null
-
     },
 
     isActive:{
-
         type:Boolean,
-
         default:true
-
     }
 
 },{
     timestamps:true
 });
 
-module.exports = mongoose.model("Masjid",MasjidSchema);
+module.exports = mongoose.model("Masjid", MasjidSchema);

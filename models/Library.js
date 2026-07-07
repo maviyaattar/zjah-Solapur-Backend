0f9fc3173860b38785770c15fcfb432ddf82f6a5
Attar Maviya Mujib <149134437+maviyaattar@users.noreply.github.com>
2026-07-07 04:24:17 -0700

    Update Library.js

--- models/Library.js
+++ models/Library.js
@@ -3,27 +3,18 @@ const mongoose = require("mongoose");
 const LibrarySchema = new mongoose.Schema({
 
     title:{
-
         type:String,
-
         required:true,
-
         trim:true
-
     },
 
     description:{
-
         type:String,
-
         default:""
-
     },
 
     category:{
-
         type:String,
-
         enum:[
             "AQEEDAH",
             "HADITH",
@@ -32,57 +23,44 @@ const LibrarySchema = new mongoose.Schema({
             "SEERAH",
             "GENERAL"
         ],
-
         default:"GENERAL"
+    },
 
+    language:{
+        type:String,
+        enum:[
+            "URDU",
+            "HINDI",
+            "ENGLISH",
+            "ARABIC",
+            "MARATHI"
+        ],
+        default:"URDU"
     },
 
     pdf:{
-
         type:String,
-
         required:true
-
     },
 
     scholar:{
-
         type:mongoose.Schema.Types.ObjectId,
-
         ref:"Scholar",
-
         required:true
-
-    },
-
-    uploadedBy:{
-
-        type:mongoose.Schema.Types.ObjectId,
-
-        ref:"User",
-
-        required:true
-
     },
 
     downloads:{
-
         type:Number,
-
         default:0
-
     },
 
     isActive:{
-
         type:Boolean,
-
         default:true
-
     }
 
 },{
     timestamps:true
 });
 
-module.exports = mongoose.model("Library",LibrarySchema);
+module.exports = mongoose.model("Library", LibrarySchema);

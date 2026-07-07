89004edec4605a289685f090b57a67b2d0d3d2a4
Attar Maviya Mujib <149134437+maviyaattar@users.noreply.github.com>
2026-07-07 04:20:58 -0700

    Update Announcement.js

--- models/Announcement.js
+++ models/Announcement.js
@@ -3,85 +3,99 @@ const mongoose = require("mongoose");
 const AnnouncementSchema = new mongoose.Schema({
 
     title:{
-
         type:String,
-
         required:true,
-
         trim:true
+    },
 
+    banner:{
+        type:String,
+        default:""
     },
 
     description:{
-
         type:String,
-
         required:true
-
     },
 
     category:{
-
         type:String,
-
         enum:[
             "GENERAL",
-            "PROGRAM",
-            "MASJID",
-            "RAMADAN",
             "EID",
-            "EMERGENCY"
+            "IJTIMAI_DUA",
+            "GOVERNMENT",
+            "NOTICE",
+            "EMERGENCY",
+            "OTHER"
         ],
-
         default:"GENERAL"
-
     },
 
-    priority:{
-
-        type:String,
-
-        enum:[
-            "LOW",
-            "MEDIUM",
-            "HIGH"
-        ],
-
-        default:"MEDIUM"
-
-    },
-
-    expiresAt:{
-
+    date:{
         type:Date,
-
         default:null
+    },
 
+    time:{
+        type:String,
+        default:""
+    },
+
+    location:{
+        type:String,
+        default:""
+    },
+
+    googleMapLink:{
+        type:String,
+        default:""
+    },
+
+    masjid:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Masjid",
+        default:null
+    },
+
+    showPopup:{
+        type:Boolean,
+        default:false
+    },
+
+    pinned:{
+        type:Boolean,
+        default:false
+    },
+
+    expiryDate:{
+        type:Date,
+        default:null
+    },
+
+    status:{
+        type:String,
+        enum:[
+            "DRAFT",
+            "PUBLISHED",
+            "EXPIRED"
+        ],
+        default:"PUBLISHED"
     },
 
     createdBy:{
-
         type:mongoose.Schema.Types.ObjectId,
-
         ref:"User",
-
         required:true
-
     },
 
     isActive:{
-
         type:Boolean,
-
         default:true
-
     }
 
 },{
     timestamps:true
 });
 
-module.exports = mongoose.model(
-    "Announcement",
-    AnnouncementSchema
-);
+module.exports = mongoose.model("Announcement", AnnouncementSchema);

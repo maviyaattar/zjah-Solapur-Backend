2014ee257a49426eeab90c345595f92c8c7a1fd4
Attar Maviya Mujib <149134437+maviyaattar@users.noreply.github.com>
2026-07-07 00:39:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const User = require("./models/User");
 
 // =====================
 // MONGODB CONNECTION

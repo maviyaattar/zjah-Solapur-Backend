d6af424db30eeccd08e76e202874c3734530e20d
Attar Maviya Mujib <149134437+maviyaattar@users.noreply.github.com>
2026-07-07 00:52:27 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,46 +2,78 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
 
-    name: {
-        type: String,
-        required: true
+    name:{
+
+        type:String,
+
+        required:true,
+
+        trim:true
+
     },
 
-    email: {
-        type: String,
-        required: true,
-        unique: true
+    email:{
+
+        type:String,
+
+        required:true,
+
+        unique:true,
+
+        lowercase:true,
+
+        trim:true
+
     },
 
-    password: {
-        type: String,
-        required: true
+    password:{
+
+        type:String,
+
+        required:true
+
     },
 
-    role: {
-        type: String,
-        enum: [
+    role:{
+
+        type:String,
+
+        enum:[
+
             "SUPER_ADMIN",
+
             "PROGRAM_ADMIN",
+
             "MASJID_ADMIN",
+
             "SCHOLAR"
+
         ],
-        default: "SCHOLAR"
+
+        required:true
+
     },
 
-    assignedMasjid: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Masjid",
-        default: null
+    assignedMasjid:{
+
+        type:mongoose.Schema.Types.ObjectId,
+
+        ref:"Masjid",
+
+        default:null
+
     },
 
-    isActive: {
-        type: Boolean,
-        default: true
+    isActive:{
+
+        type:Boolean,
+
+        default:true
+
     }
 
-}, {
-    timestamps: true
+},{
+    timestamps:true
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User",UserSchema);
